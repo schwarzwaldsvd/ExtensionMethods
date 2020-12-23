de9245fe7633d6c1044d395c6d226d51a0f2fe27
schwarzwaldsvd <svartskog@gmail.com>
2020-12-23 17:56:38 -0500

    Add project files.

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = "FirstName";
+            // using extension method syntax
+            string result = name.ToggleFirstLetterCase();
+
+            // using wrapper class syntax
+            //string result = StringHelper.ToggleFirstLetterCase(name);
+            Console.WriteLine(result);
+            
+
+            List<int> numbers = new List<int>{ 1,2,3,4,5,6,7,8,9,10 };
+            IEnumerable<int> evenNumbers = numbers.Where(n => n % 2 == 0);
+            IEnumerable<int> evenNumbersActual = Enumerable.Where(numbers, n => n % 2 == 0); 
+            
+            foreach (int evenNumber in evenNumbers)
+            {
+                Console.WriteLine(evenNumber);
+            }
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ ExtensionMethods/StringHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    // 1. add 'static' keyword to the class in question
+    // 2. 'this' keyword should preceed the type we are extending in the input parameter,
+    // and it should be passed as the fist parameter to the function
+    public static class StringHelper
+    {
+        public static string ToggleFirstLetterCase(this string inputString )
+        {
+            if (inputString.Length <= 0) return inputString;
+            var charArray = inputString.ToCharArray();
+            charArray[0] = char.IsUpper(charArray[0]) ? char.ToLower(charArray[0]) : char.ToUpper(charArray[0]);
+            return new string(charArray);
+
+        }
+    }
+}
